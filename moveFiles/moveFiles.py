2c44f3a9728f53df2aac966d6aa52d51e52b383d
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2017-11-15 21:42:52 -0800

    update moveFiles.py applying DRY
    update moveFiles.py applying DRY

--- moveFiles/moveFiles.py
+++ moveFiles/moveFiles.py
@@ -1,22 +1,18 @@
 import shutil
+import os
 
-origin_random_1 = '/Users/sethjohnson/Desktop/folder-a/random_1.txt'
-dest_random_1 = '/Users/sethjohnson/Desktop/folder-b/random_1.txt'
-origin_random_2 = '/Users/sethjohnson/Desktop/folder-a/random_2.txt'
-dest_random_2 = '/Users/sethjohnson/Desktop/folder-b/random_2.txt'
-origin_random_3 = '/Users/sethjohnson/Desktop/folder-a/random_3.txt'
-dest_random_3 = '/Users/sethjohnson/Desktop/folder-b/random_3.txt'
-origin_random_4 = '/Users/sethjohnson/Desktop/folder-a/random_4.txt'
-dest_random_4 = '/Users/sethjohnson/Desktop/folder-b/random_4.txt'
 
-shutil.move(origin_random_1, dest_random_1)
-print("Your file has been moved to ", dest_random_1)
+origin_random_1 = '/Users/sethjohnson/Desktop/folder-a/'
+dest_random_1 = '/Users/sethjohnson/Desktop/folder-b/'
 
-shutil.move(origin_random_2, dest_random_2)
-print("Your file has been moved to ", dest_random_2)
+contents_a = os.listdir(origin_random_1)
 
-shutil.move(origin_random_3, dest_random_3)
-print("Your file has been moved to ", dest_random_3)
 
-shutil.move(origin_random_4, dest_random_4)
-print("Your file has been moved to ", dest_random_4)
+for files in contents_a:
+    if files.endswith('.txt'):
+        shutil.move(os.path.join(origin_random_1, files), os.path.join(dest_random_1, files))
+        
+    
+        
+
+
